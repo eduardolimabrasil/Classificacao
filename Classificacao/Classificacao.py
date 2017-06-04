1913bd01ab918344a9d3c3fa11bd8a5034f2dbe4
Eduardo <eduardosalgueirolima@gmail.com>
2017-06-04 13:08:04 -0300

    Erros e Acertos

--- Classificacao/Classificacao.py
+++ Classificacao/Classificacao.py
@@ -1,16 +1,26 @@
-from sklearn.naive_bayes import MultinomialNB
+﻿from sklearn.naive_bayes import MultinomialNB
 
-cancer = [[1,0,1],
-          [1,0,0],
-          [1,1,0],
-          [0,0,1],
-          [1,0,1],
-          [1,1,1]]
+try:
+    print "Inicio"
+    cancer = [[1,0,1],
+              [1,0,0],
+              [1,1,0],
+              [0,0,1],
+              [1,0,1],
+              [1,1,1]]
+    
+    marcacoes = [1,1,1,-1,-1,-1]
+    amostra =[[0,0,0],[1,1,1],[1,0,0],[0,1,0]]
 
-marcacoes = [1,1,1,0,0,0]
-amostra =[0,0,0]
-modelo = MultinomialNB()
-
-modelo.fit(cancer,marcacoes)
-print modelo.predict(amostra)
-
+    modelo = MultinomialNB()
+    
+    modelo.fit(cancer,marcacoes)
+    resultado = modelo.predict(amostra)
+    marcacoes_testes = [-1,-1,1,-1]
+    diferencas = resultado - marcacoes_testes
+    print str(diferencas)
+    acertos = [d for d in diferencas if d==0]
+    print str(len(acertos))
+    len(amostra)
+except Exception as e:
+    print str(e)
